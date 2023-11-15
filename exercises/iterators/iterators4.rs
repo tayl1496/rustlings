abe758a62713529f0258702f591f82cf085c4f00
Sam Taylor <stsam63@gmail.com>
2023-11-14 21:39:23 -0600

    iterators lets go

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,9 +1,12 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    if num == 0 {
+        return 1;
+    }
+    (1..=num).into_iter().fold(1, |sum, x| sum * x)
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
