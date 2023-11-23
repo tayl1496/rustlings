598468f4c6f0a874b6aa44cf50a2229fd581ca50
Sam Taylor <stsam63@gmail.com>
2023-11-23 12:03:28 -0600

    finished :)

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut iter = s.split(",");
+        if iter.clone().count() != 2 {
+            return Person::default();
+        }
+        if let Some(name) = iter.next() {
+            if name == "" {
+                return Person::default();
+            }
+            if let Some(age) = iter.next() {
+                let parsed_age = age.parse::<usize>();
+                match parsed_age {
+                    Ok(age) => {
+                        return Person {
+                            name: name.to_owned(),
+                            age,
+                        }
+                    }
+                    Err(_) => return Person::default(),
+                }
+            }
+        }
+        Person::default()
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,34 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let mut iter = s.split(",");
+        if iter.clone().count() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let Some(name) = iter.next() else {
+            return Err(ParsePersonError::NoName);
+        };
+        if name == "" {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let mut age: usize = 0;
+        if let Some(pos_age) = iter.next() {
+            match pos_age.parse::<usize>() {
+                Ok(a) => age = a,
+                Err(e) => return Err(ParsePersonError::ParseInt(e)),
+            }
+        }
+
+        return Ok(Person {
+            age: age,
+            name: name.to_owned(),
+        });
     }
 }
 
